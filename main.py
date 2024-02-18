623d83c79675e2bea2e1efb89068796c3151b142
Scheggetta <edoardo.fusa@gmail.com>
2024-02-18 12:30:20 +0100

    Developing first baseline

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import torch as pt
+import torch
 import numpy as np
 import json
 import os
@@ -27,8 +28,42 @@ if not (Path('Dataset/Train_Queries').exists() and Path('Dataset/Train_Evidence'
                             Path.joinpath(Path('Dataset/Train_Evidence'), Path(f)))
 
 
+
+# q = (1, 2, 3)
+# e = (2, 3, 3)
+# q = torch.Tensor([[[1, 2, 1], [1, 2, 1]]])
+# e = torch.Tensor([[[-1, 2, 3], [-2, -1, 1], [0, 0, 1]], [[-1, -1, -1], [0, -1, 1], [0, 0, 0]]])
+# x = torch.matmul(q, torch.transpose(e, 1, 2))
+#
+# x2 = torch.einsum('ijk,lmk->jlm', [q, e])
+#
+# q = torch.rand(1, 512, 768)
+# e = torch.rand(10, 512, 768)
+# x = torch.matmul(q, torch.transpose(e, 1, 2))
+# x2 = torch.einsum('ijk,ljk->l', [q, e])
+#
+# q = torch.rand(1, 512, 768)
+# e = torch.rand(10, 512, 768)
+# q = torch.mean(q, dim=1)
+# e = torch.mean(e, dim=1)
+# cos = torch.nn.CosineSimilarity()
+# x = cos(q, e)
+#
+# # q = (1, 5)
+# # e = (3, 5)
+# q = torch.Tensor([[1, 2, 3, 4, 5]])
+# attn = torch.Tensor([1, 1, 0, 0, 0])
+# q = q * attn
+# q = torch.squeeze(q)
+# e = torch.Tensor([[1, 2, -1, 4, -2], [1, -1, 3, 4, -1], [-1, -1, -1, -1, 5]])
+# x = torch.matmul(q, torch.transpose(e, 0, 1))
+# x = cos(q, e)
+
+
+
 Roby = AutoModel.from_pretrained('roberta-base')
 tokenizer = AutoTokenizer.from_pretrained('roberta-base')
+q_embed = tokenizer([open(Path.joinpath(Path('Dataset/Train_Queries'), x)).read() for x in os.listdir('Dataset/Train_Queries')], padding=True, truncation=True, max_length=512, return_tensors='pt')
 e_embed = tokenizer([open(Path.joinpath(Path('Dataset/Train_Evidence'), x)).read() for x in os.listdir('Dataset/Train_Evidence')], padding=True, truncation=True, max_length=512, return_tensors='pt')
 
 
@@ -41,9 +76,9 @@ def take_first_k(embed, k):
     embed['attention_mask'] = embed['attention_mask'][:k]
     return embed
 
+q_embed = take_first_k(q_embed, 1)
 e_embed = take_first_k(e_embed, 10)
 model = Model('roberta-base')
-result = model(e_embed)
-
+result = model(q_embed, e_embed)
 
 pass
--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 from torch import nn
 import torch
-from transformers import AutoModel, BertModel, RobertaModel
+from transformers import AutoModel
 
 
 class Model(nn.Module):
@@ -9,17 +9,23 @@ class Model(nn.Module):
         self.embedder = AutoModel.from_pretrained(name)
         for param in self.embedder.parameters():
             param.requires_grad = False
+
+        self.cos = torch.nn.CosineSimilarity()
         # self.linear = nn.Linear(head_size, 4)
         # self.tanh = nn.Tanh()
 
     def forward(self, query, evidence):
-        # attention_mask = x['attention_mask'].unsqueeze(-1)
+        attention_mask_q = query['attention_mask'].unsqueeze(-1)
+        attention_mask_e = evidence['attention_mask'].unsqueeze(-1)
         q = self.embedder(**query).last_hidden_state
         e = self.embedder(**evidence).last_hidden_state
-        # x = x * attention_mask
-        # x = x.mean(dim=1)
-        # x = self.linear(x)
-        # x = (self.tanh(x) + 1) / 2
-        # Dot product
-        x = torch.matmul(q, e)
+        q = q * attention_mask_q
+        e = e * attention_mask_e
+
+        x2 = torch.einsum('ijk,ljk->l', [q, e])
+
+        q = q.mean(dim=1)
+        e = e.mean(dim=1)
+        x = self.cos(q, e)
+
         return x
