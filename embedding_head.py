19066ab8619483ce5739b189a9d6041695db6161
Scheggetta <edoardo.fusa@gmail.com>
2024-03-07 15:11:56 +0100

    Modules refactoring

--- embedding_head.py
+++ embedding_head.py
@@ -1,26 +1,23 @@
 import os
-import pickle
 import json
 import random
-import copy
 from math import ceil
 from pathlib import Path
+from datetime import datetime
 
 import numpy as np
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader
 from torcheval.metrics.functional import binary_f1_score
 from tqdm import tqdm
 
-from setlist import SetList
-from datetime import datetime
+from parameters import *
+from dataset import TrainingDataset, QueryDataset, DocumentDataset, custom_collate_fn, get_gpt_embeddings
 
 
 # TODO:
 #  - focal loss
 #  - hyperparameter grid search
-#  - better prediction function
-#  - f1 score metric
 #  - alternative model architecture
 
 
@@ -28,19 +25,6 @@ seed = 62
 random.seed(seed)
 np.random.seed(seed)
 torch.manual_seed(seed)
-
-
-EMB_IN = 1536
-EMB_OUT = 50           # 50
-SAMPLE_SIZE = 15       # 15
-TEMPERATURE = 1.0
-PE_WEIGHT = None
-DYNAMIC_CUTOFF = False  # False
-
-# Cutoff hyperparameters
-PE_CUTOFF = 5          # 5
-MAX_DOCS = 10
-RATIO_MAX_SIMILARITY = 0.8
 
 
 class EmbeddingHead(torch.nn.Module):
@@ -92,150 +76,7 @@ class EmbeddingHead(torch.nn.Module):
         self.load_state_dict(torch.load(file_path))
 
 
-class TrainingDataset(Dataset):
-    def __init__(self, embeddings: dict, json_dict: dict):
-        self.embeddings = embeddings
-        self.json_dict = json_dict
 
-        queries_names = [key for key in json_dict if key in embeddings]
-        self.all_evidences = sorted(list(set([evidence for query_name in queries_names for evidence in json_dict[query_name]])))
-
-        self.queries = []
-        for q_name in queries_names:
-            self.queries.append((q_name, embeddings[q_name]))
-
-        self.evidences = []
-        for q_name in queries_names:
-            single_evidences = torch.empty((0, EMB_IN))
-            evidences = json_dict[q_name]
-            for e_name in evidences:
-                single_evidences = torch.cat((single_evidences, embeddings[e_name].unsqueeze(0)), dim=0)
-            self.evidences.append((evidences, single_evidences))
-
-    def __len__(self):
-        return len(self.queries)
-
-    def __getitem__(self, index):
-        query_name = self.queries[index][0]
-        query = self.queries[index][1]
-        evidence_names = self.evidences[index][0]
-        evidence = self.evidences[index][1]
-
-        excluded_documents = evidence_names.copy()
-        excluded_documents.append(query_name)
-
-        sample_space = self.all_evidences.copy()
-        for el in excluded_documents:
-            try:
-                sample_space.remove(el)
-            except ValueError:
-                pass
-
-        negative_evidences_names = random.sample(list(sample_space), SAMPLE_SIZE)
-
-        negative_evidences = torch.empty((0, EMB_IN))
-        for e_name in negative_evidences_names:
-            negative_evidences = torch.cat((negative_evidences, self.embeddings[e_name].unsqueeze(0)), dim=0)
-
-        return query, evidence, negative_evidences
-
-
-class QueryDataset(Dataset):
-    def __init__(self, embeddings: dict, json_dict: dict):
-        self.embeddings = embeddings
-        self.json_dict = json_dict
-
-        queries_names = [key for key in json_dict if key in embeddings]
-        self.queries = [(q_name, embeddings[q_name]) for q_name in queries_names]
-
-    def __len__(self):
-        return len(self.queries)
-
-    def __getitem__(self, index):
-        return self.queries[index]
-
-
-class DocumentDataset(Dataset):
-    def __init__(self, embeddings: dict, json_dict: dict):
-        self.embeddings_dict = embeddings
-        self.json_dict = json_dict
-
-        self.embeddings = []
-        self.masked_query = None
-        self.masked_evidences = None
-
-        for key in embeddings:
-            self.embeddings.append((key, embeddings[key]))
-
-    def __len__(self):
-        return len(self.embeddings)
-
-    def mask(self, query_name):
-        if self.masked_query:
-            raise ValueError('A document is already masked')
-        q_embedding = self.embeddings_dict[query_name]
-        self.masked_evidences = self.json_dict[query_name]
-
-        self.masked_query = (query_name, q_embedding)
-        self.embeddings.remove((query_name, q_embedding))
-
-    def restore(self):
-        if self.masked_query:
-            self.embeddings.append(self.masked_query)
-            self.masked_query = None
-            self.masked_evidences = None
-        else:
-            raise ValueError('No element to restore')
-
-    def __getitem__(self, index):
-        return self.embeddings[index]
-
-    def get_indexes(self, filenames):
-        return [self.embeddings.index((x, self.embeddings_dict[x])) for x in filenames]
-
-
-def custom_collate_fn(batch: list):
-    queries = torch.stack([x[0] for x in batch])
-    evidences = [x[1] for x in batch]
-    negative_evidences = torch.stack([x[2] for x in batch])
-
-    return queries, evidences, negative_evidences
-
-
-def get_gpt_embeddings(folder_path: str, selected_dict: dict):
-    embeddings = {}
-
-    files = []
-    for key, values in selected_dict.items():
-        files.append(key)
-        files.extend(values)
-    files = SetList(files)
-
-    # Check if any selected evidence does not have a related query
-    for file in files:
-        if file in selected_dict:
-            # `file` is a query
-            evidences = selected_dict[file]
-            assert any(evidence in files for evidence in evidences), f'No evidence for query {file}'
-        else:
-            # `file` is an evidence
-            found_query = False
-            for key in selected_dict:
-                if file in selected_dict[key]:
-                    found_query = True
-                    break
-            assert found_query, f'No query for evidence {file}'
-
-    for file in files:
-        with open(os.path.join(folder_path, file), 'rb') as f:
-            e = pickle.load(f)
-            n_paragraphs = len(e) // EMB_IN
-            assert len(e) % EMB_IN == 0
-
-            e = torch.tensor(e).view(n_paragraphs, EMB_IN).mean(dim=0)
-            embeddings[file] = e
-
-    return embeddings
 
 
 def train(model, train_dataloader, validation_dataloader, num_epochs, save_weights=True):
---
+++ parameters.py
@@ -0,0 +1,11 @@
+EMB_IN = 1536
+EMB_OUT = 50           # 50
+SAMPLE_SIZE = 15       # 15
+TEMPERATURE = 1.0
+PE_WEIGHT = None
+DYNAMIC_CUTOFF = False  # False
+
+# Cutoff hyperparameters
+PE_CUTOFF = 5          # 5
+MAX_DOCS = 10
+RATIO_MAX_SIMILARITY = 0.8
