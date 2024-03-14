ecdb3452a3414a0412f9dc4c465962b7691d60de
Scheggetta <edoardo.fusa@gmail.com>
2024-03-14 10:50:13 +0100

    CrossEntropyLoss for constrastive loss

--- parameters.py
+++ parameters.py
@@ -10,7 +10,7 @@ HIDDEN_UNITS = 256      # 256
 
 # Torch Dataset
 # TODO: maybe the negative samples should be taken from the most similar documents according to BM25 (and also increase sample size)
-SAMPLE_SIZE = 400        # 15 - Negative samples per query
+SAMPLE_SIZE = 15        # 15 - Negative samples per query
 
 # Loss function
 PE_WEIGHT = 0.5          # None
@@ -21,7 +21,7 @@ COSINE_LOSS_MARGIN = 0.0  # 0.5
 LR = 0.001          # 0.001
 FACTOR = 0.1        # 0.1
 THRESHOLD = 0.001    # 0.001
-PATIENCE = 3        # 3
+PATIENCE = 5        # 3
 COOLDOWN = 3        # 3
 
 # Cutoff hyperparameters
